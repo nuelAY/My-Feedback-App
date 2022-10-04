7fadf591254b1512fb3ab5ee7332f2bf8c24b864
Manuel <nuelay99@gmail.com>
2022-10-04 01:10:50 -0700

    first commit

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import FeedbackItem from './FeedbackItem'
+import { useContext } from 'react'
+//import PropTypes from 'prop-types'
+import {motion, AnimatePresence} from 'framer-motion'
+import FeedbackContext from '../context/FeedbackContext'
+
+
+function FeedbackList() {
+    const {feedback} = useContext(FeedbackContext)
+    if (!feedback || feedback.length === 0) {
+        return <p>No Feedback Yet</p>
+    }
+  return (
+    <div className='feedback-list'>
+        <AnimatePresence>
+            {feedback.map((item) => (
+                <motion.div
+                    key={item.id}
+                    initial={{ opacity: 0 }}
+                    animate={{opacity:1}}
+                    exit={{opacity:0}}
+                >
+                    <FeedbackItem key={item.id}
+                item={item}
+                /*handleDelete = {handleDelete}*/  />
+              </motion.div>
+            
+            ))}
+        </AnimatePresence>
+    </div>
+  )
+}
+
+/*FeedbackList.propTypes = {
+    feedback: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            rating: PropTypes.number.isRequired,
+        })
+    )
+}*/
+
+export default FeedbackList
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import PropTypes from 'prop-types'
+
+function Header() {
+    return (
+        <header>
+            <div className="container">
+                <h2>Feedback UI</h2>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
